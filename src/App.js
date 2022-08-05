7267bcd9ea14ca679d4470ec3030a432fc42cd40
ivannikovlg13 <livannikov55@gmail.com>
2022-08-06 01:58:58 +0300

    added props for items

--- src/App.js
+++ src/App.js
@@ -1,4 +1,67 @@
 import { Header, CardItem, Drawer } from './components';
+
+const items = [
+  {
+    id: 1,
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: '12 999 руб.',
+    imageUrl: './img/sneakers/1.jpg',
+  },
+  {
+    id: 2,
+    name: 'Мужские Кроссовки Nike Air Max 270',
+    price: '12 999 руб.',
+    imageUrl: './img/sneakers/2.jpg',
+  },
+  {
+    id: 3,
+    name: 'Мужские Мужские Кроссовки Under Armour Curry 8',
+    price: '8 499 руб.',
+    imageUrl: './img/sneakers/3.jpg',
+  },
+  {
+    id: 4,
+    name: 'Кроссовки Puma X Aka Boku Future Rider',
+    price: '9 499 руб.',
+    imageUrl: './img/sneakers/4.jpg',
+  },
+  {
+    id: 5,
+    name: 'Мужские Кроссовки Nike Kyrie 7',
+    price: '13 499 руб.',
+    imageUrl: './img/sneakers/5.jpg',
+  },
+  {
+    id: 6,
+    name: 'Мужские Кроссовки Jordan Air Jordan 11',
+    price: '6 499 руб.',
+    imageUrl: './img/sneakers/6.jpg',
+  },
+  {
+    id: 7,
+    name: 'Мужские Кроссовки Nike LeBron XVIII',
+    price: '8 449 руб.',
+    imageUrl: './img/sneakers/7.jpg',
+  },
+  {
+    id: 8,
+    name: 'Мужские Кроссовки Nike Lebron XVIII Low',
+    price: '22 469 руб.',
+    imageUrl: './img/sneakers/8.jpg',
+  },
+  {
+    id: 9,
+    name: 'Мужские Кроссовки Nike Blazer Mid Suede',
+    price: '20 499 руб.',
+    imageUrl: './img/sneakers/9.jpg',
+  },
+  {
+    id: 10,
+    name: 'Мужские Кроссовки Nike Kyrie Flytrap IV',
+    price: '19 499 руб.',
+    imageUrl: './img/sneakers/10.jpg',
+  },
+];
 function App() {
   return (
     <div className="wrapper clear">
@@ -14,11 +77,9 @@ function App() {
         </div>
 
         <div className="card__items">
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
-          <CardItem />
+          {items.map((item) => (
+            <CardItem key={item.id} {...item} />
+          ))}
         </div>
       </section>
     </div>
--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-function CardItem() {
+function CardItem({ name, price, imageUrl }) {
   return (
     <div className="card__item mb-40">
       <img className="imgBtn" src="./img/icons/unliked-btn.svg" alt="unliked" />
-      <img width={133} src="./img/sneakers/1.jpg" alt="item" />
-      <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+      <img width={133} src={imageUrl} alt="item" />
+      <p>{name}</p>
       <div className="d-flex justify-between align-center info__box mt-20">
         <div className="d-flex flex-column">
           <span className="price text-uppercase">Цена:</span>
-          <b>12999грн.</b>
+          <b>{price}</b>
         </div>
         <img className="imgBtn" src="./img/icons/add-btn.svg" alt="add" />
       </div>
